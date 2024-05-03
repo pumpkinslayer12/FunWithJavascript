9678529af0d2bf44dccc3f5d46d9da7cb5375f7a
Kris Rutherford <klruther1@gmail.com>
2024-05-03 13:57:03 -0400

    Initial updates

---
+++ js/beginner.js
@@ -0,0 +1,6 @@
+let mainContentID = document.getElementById("content-div");
+
+const gub = "I'm a string";
+let gub2 = "I'm also a string";
+gub2 = "I'm a string now";
+mainContentID.innerHTML = gub+gub2;
