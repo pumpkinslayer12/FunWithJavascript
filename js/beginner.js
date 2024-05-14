ef1fd1072c27abaff72bdde9f82c319c47fbe8c9
Kris Rutherford <klruther1@gmail.com>
2024-05-14 18:48:04 -0400

    vgdsf

--- js/beginner.js
+++ js/beginner.js
@@ -1,6 +1,5 @@
 let mainContentID = document.getElementById("content-div");
 
 const gub = "I'm a string";
-let gub2 = "I'm also a string";
-gub2 = "I'm a string now";
-mainContentID.innerHTML = gub+gub2;
+let gub2 = "eeI'm also a stringee";
+mainContentID.innerHTML = `Give me my fucking: ${gub}`;
